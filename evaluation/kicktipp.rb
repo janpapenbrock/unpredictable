1760e46a0bad6bfc2ed70852c209696790f0c457
janpapenbrock <lunoxx@gmail.com>
2013-09-01 14:44:53 +0200

    Implement Kicktipp evaluation algorithm.

--- evaluation/kicktipp.rb
+++ evaluation/kicktipp.rb
@@ -2,12 +2,42 @@ module Evaluation
 
 	class Kicktipp
 
+		attr_accessor :prediction, :reality
+
 		def initialize prediction, reality
 			@prediction = prediction
 			@reality = reality
 		end
 
 		def evaluate
+			return 4 if predicted_result?
+			return 3 if predicted_difference?
+			return 2 if predicted_tendency?
+			0
+		end
+
+		def predicted_result?
+			[:home_goals, :away_goals].each do |property|
+				return false unless @prediction[property] == @reality[property]
+			end
+			true
+		end
+
+		def predicted_difference?
+			return false if predicted_result?
+			
+			return calc_goal_differences.uniq.one?
+		end
+
+		def predicted_tendency?
+			return false if predicted_result?
+			return false if predicted_difference?
+
+			return (calc_goal_differences.reduce(:*) > 0)
+		end
+
+		def calc_goal_differences
+			[@prediction, @reality].collect{|result| result[:home_goals] - result[:away_goals]}
 		end
 
 	end
--- spec/evaluation/kicktipp_spec.rb
+++ spec/evaluation/kicktipp_spec.rb
@@ -3,7 +3,8 @@ require_relative "../spec_helper"
 describe Evaluation::Kicktipp do
 
 	before :each do
-		prediction = reality = { home_goals: 0, away_goals: 0 }
+		prediction = { home_goals: 0, away_goals: 0 }
+		reality = prediction.clone
 		@evaluation = Evaluation::Kicktipp.new prediction, reality
 	end
 
@@ -22,16 +23,75 @@ describe Evaluation::Kicktipp do
 
 		it "returns 4 points for exact result if home team wins" do
 			results = { home_goals: 1, away_goals: 0}
-			evaluation = Evaluation::Kicktipp.new results, results
+			evaluation = Evaluation::Kicktipp.new results, results.clone
 			evaluation.evaluate.should eql 4
 		end
 
 		it "returns 4 points for exact result if away team wins" do
 			results = { home_goals: 1, away_goals: 2}
-			evaluation = Evaluation::Kicktipp.new results, results
+			evaluation = Evaluation::Kicktipp.new results, results.clone
 			evaluation.evaluate.should eql 4
 		end
 
+	end
+
+	describe "#calc_goal_differences" do
+		it "calculates goal differences for both prediction and reality" do
+			prediction = { home_goals: 0, away_goals: 0 }
+			reality = 	 { home_goals: 1, away_goals: 2 }
+			evaluation = Evaluation::Kicktipp.new prediction, reality
+			differences = evaluation.calc_goal_differences
+			differences.first.should be 0
+			differences.last.should be -1
+		end
+	end
+
+	it "recognizes the exact match of prediction and reality" do
+		@evaluation.predicted_result?.should be_true
+	end
+
+	it "recognizes that prediction and reality do not match" do
+		@evaluation.reality[:home_goals] += 1
+		@evaluation.predicted_result?.should be_false
+	end
+
+	it "recognizes the correct goal difference of prediction compared to reality" do
+		@evaluation.prediction = { home_goals: 1, away_goals: 1 }
+		@evaluation.predicted_difference?.should be_true
+	end
+
+	it "recognizes an exact match over the same goal difference" do
+		@evaluation.predicted_difference?.should be_false
+	end
+
+	it "recognizes the correct tendency of prediction compared to reality" do
+		prediction = { home_goals: 2, away_goals: 0 }
+		reality = { home_goals: 5, away_goals: 0 }
+		evaluation = Evaluation::Kicktipp.new prediction, reality
+		evaluation.predicted_tendency?.should be_true
+	end
+
+	it "recognizes an incorrect tendency when comparing remis to victory" do
+		prediction = { home_goals: 0, away_goals: 0 }
+		reality = { home_goals: 5, away_goals: 0 }
+		evaluation = Evaluation::Kicktipp.new prediction, reality
+		evaluation.predicted_tendency?.should be_false
+	end
+
+	it "recognizes an incorrect tendency" do
+		prediction = { home_goals: 0, away_goals: 2 }
+		reality = { home_goals: 5, away_goals: 0 }
+		evaluation = Evaluation::Kicktipp.new prediction, reality
+		evaluation.predicted_tendency?.should be_false
+	end
+
+	it "recognizes an exact match over tendency" do
+		@evaluation.predicted_tendency?.should be_false
+	end
+
+	it "recognizes goal difference over tendency" do
+		@evaluation.prediction = { home_goals: 1, away_goals: 1 }
+		@evaluation.predicted_tendency?.should be_false
 	end
 
 end
