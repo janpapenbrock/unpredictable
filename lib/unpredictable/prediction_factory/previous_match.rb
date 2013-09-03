0b0ab0d7dfbd62133c636e289b1d170515466521
janpapenbrock <lunoxx@gmail.com>
2013-09-03 21:17:08 +0200

    Further implementation of previous match prediction algorithm.

--- lib/unpredictable/prediction_factory/previous_match.rb
+++ lib/unpredictable/prediction_factory/previous_match.rb
@@ -1,4 +1,11 @@
 module PredictionFactory
 	class PreviousMatch
+
+		def predict match
+
+			raise ArgumentError unless match.is_a? Match
+
+		end
+
 	end
 end
--- spec/prediction_factory/previous_match_spec.rb
+++ spec/prediction_factory/previous_match_spec.rb
@@ -2,11 +2,27 @@ require_relative "../spec_helper"
 
 describe PredictionFactory::PreviousMatch do
 
+	before :each do 
+		@factory = PredictionFactory::PreviousMatch.new
+		@match = Match.new
+		@team1_matches = [ Match.new, @match ]
+		@team2_matches = [ Match.new, @match ]
+	end
+
 	describe "#predict" do
+
+		it "throws an ArgumentError if given less than one parameter" do
+			lambda { @factory.predict() }.should raise_exception ArgumentError
+		end
+
+		it "throws an ArgumentError if first parameter is not a match instance" do
+			lambda { @factory.predict( [] ) }.should raise_exception ArgumentError
+		end
 
 		context "given a valid match with previous matches for both teams" do
 
 			it "returns a prediction" do
+				@factory.predict.should be_instance_of Prediction
 			end
 
 		end
@@ -14,6 +30,7 @@ describe PredictionFactory::PreviousMatch do
 		context "given a match without previous matches for both teams" do
 
 			it "returns nil" do
+				@factory.predict.should be_nil
 			end
 
 		end
