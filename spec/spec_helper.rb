4f6f44728404f42d986d5c261119b6406eaec538
janpapenbrock <lunoxx@gmail.com>
2013-09-03 23:04:45 +0200

    Move some classes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'coveralls'
 Coveralls.wear!
 
-require_relative "../lib/unpredictable/match"
-require_relative "../lib/unpredictable/prediction"
+require_relative "../lib/unpredictable/core/match"
+require_relative "../lib/unpredictable/core/prediction"
 require_relative "../lib/unpredictable/evaluation/kicktipp"
 require_relative "../lib/unpredictable/prediction_factory/previous_match"
