ab6912e8e24143177847832cb878b87a1ac847ff
janpapenbrock <lunoxx@gmail.com>
2013-09-04 07:52:13 +0200

    Add team class.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,6 @@ Coveralls.wear!
 
 require_relative "../lib/unpredictable/core/match"
 require_relative "../lib/unpredictable/core/prediction"
+require_relative "../lib/unpredictable/core/team"
 require_relative "../lib/unpredictable/evaluation/kicktipp"
 require_relative "../lib/unpredictable/prediction_factory/previous_match"
