844fb6c530cb4d0da8517cbbdcef1ebd190c4436
janpapenbrock <lunoxx@gmail.com>
2013-09-15 18:53:00 +0200

    Implement team match list logic.

--- lib/unpredictable/core/team.rb
+++ lib/unpredictable/core/team.rb
@@ -1,2 +1,26 @@
 class Team
+
+	attr_accessor :matches
+
+	def initialize matches=[]
+		@matches = matches
+	end
+
+	def add_match match
+		@matches << match
+		@matches.uniq!
+		@matches
+	end
+
+	def match_before match
+		match_before_date match.date
+	end
+
+	def match_before_date date
+		datetime = date.to_datetime
+		matches_before = @matches.select{|match| match.date.to_datetime < datetime}
+		matches_before.sort! { |a,b| a.date <=> b.date }
+		matches_before.last
+	end
+
 end
--- spec/unpredictable/core/team_spec.rb
+++ spec/unpredictable/core/team_spec.rb
@@ -2,4 +2,94 @@ require "spec_helper"
 
 describe Team do
 	
+	before :each do
+		@match1 = double("match", date: Time.new(2008,1,1, 13,30,0, "+02:00"))
+		@match2 = double("match", date: Time.new(2008,1,15, 13,30,0, "+02:00"))
+		@match3 = double("match", date: Time.new(2008,1,29, 13,30,0, "+02:00"))
+		@team = Team.new( [@match1, @match2] )
+	end
+
+	describe "#new" do
+
+		it "should accept a list of matches" do
+			team = Team.new( [ @match1 ])
+			team.matches.count.should eql 1
+			team.matches.should include @match1
+		end
+
+	end
+
+	it "should keep a list of matches played" do
+		@team.matches.should include @match1
+		@team.matches.count.should eql 2
+	end
+
+	describe "#add_match" do
+
+		it "should raise an ArgumentError if given no arguments" do
+			lambda { @team.add_match }.should raise_exception ArgumentError
+		end
+
+		it "should return the new list of matches" do
+			match = double("match")
+			@team.add_match(match).should be_instance_of Array
+		end
+
+		it "should accept a match to add to the list of matches" do
+			match = double("match")
+			@team.add_match(match).should include match
+		end
+
+		it "should add a match once only" do
+			match = double("match")
+			count = @team.add_match(match).count
+			@team.add_match(match).count.should eql count
+		end
+
+	end
+
+	describe "#match_before" do
+
+		it "should return the match played before a specific match" do
+			@team.match_before(@match3).should == @match2
+		end
+
+		it "should return nil if there was no match played before a specific match" do
+			@team.match_before(@match1).should be_nil
+		end
+
+		it "should return nil if the team has not played any matches" do
+			team = Team.new
+			team.match_before(@match1).should be_nil
+		end
+
+		it "should return the same result as calling match_before_date" do
+			before_date_result = @team.match_before_date(@match3.date)
+			@team.match_before(@match3).should == before_date_result
+		end
+
+	end
+
+	describe "#match_before_date" do
+
+		it "should return the match played before a specific date" do
+			@team.match_before_date(@match3.date).should == @match2
+		end
+
+		it "should return nil if no match was played before a specific date" do
+			@team.match_before_date(@match1.date).should be_nil
+		end
+
+		it "should return nil if the team has not played any matches" do
+			team = Team.new
+			team.match_before_date(@match1.date).should be_nil
+		end
+
+		it "should return the same result as calling match_before" do
+			before_result = @team.match_before(@match3)
+			@team.match_before_date(@match3.date).should == before_result
+		end
+
+	end
+
 end
