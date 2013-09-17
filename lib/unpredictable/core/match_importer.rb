5656988c227659ec61496248f1d6266c355df0af
janpapenbrock <lunoxx@gmail.com>
2013-09-17 22:03:09 +0200

    Begin implementation of importing matches from API.

--- lib/unpredictable/core/match_importer.rb
+++ lib/unpredictable/core/match_importer.rb
@@ -1,9 +1,51 @@
-class MatchImporter
-	
-	def self.import season
+require 'json'
 
-		
-		
+class MatchImporter
+
+	API_BASE = "http://openligadb-json.heroku.com/api/"
+
+	attr_reader :league_code, :season
+
+	def initialize league_code, season
+		@league_code = league_code
+		@season = season
 	end
+
+	def teams
+		return @teams if @teams
+		@teams = fetch_teams
+		@teams
+	end
+
+	def matches
+		return @matches if @matches
+		@matches = []
+		@matches
+	end
+
+	def fetch_teams
+		method = "teams_by_league_saison"
+		team_data = request method
+		parse_teams team_data
+	end
+
+	def parse_teams team_json
+		return [] unless team_json.key? "team"
+		team_json["team"].collect do |team_data|
+			Team.new(team_data["team_name"])
+		end
+	end
+
+	def request method, params={}
+		JSON.parse("{}")
+	end
+
+	def request_params params={}
+		{
+			league_saison:   @season,
+			league_shortcut: @league_code
+		}.merge(params)
+	end
+
 
 end
--- spec/unpredictable/core/match_importer_spec.rb
+++ spec/unpredictable/core/match_importer_spec.rb
@@ -2,16 +2,67 @@ require "spec_helper"
 
 describe MatchImporter do
 
-	describe "#import" do
+	before :each do
+		@importer = MatchImporter.new("bl1", 2012)
+	end
 
-		it "should raise an ArgumentError if called without any arguments" do
-			lambda { MatchImporter.import }.should raise_exception ArgumentError
+	describe "#new" do
+
+		it "should raise an ArgumentError if called with less than two arguments" do
+			lambda { MatchImporter.new("any")}.should raise_exception ArgumentError
 		end
 
-		it "should return a list of matches if given a season year" do
-			# MatchImporter.import(2012).should be_instance_of Array
+		it "should accept a league code and a season" do
+			importer = MatchImporter.new("bl1", 2012)
+			importer.league_code.should eql "bl1"
+			importer.season.should eql 2012
 		end
 
+	end
+
+	describe "#teams" do
+		it "should return a list of teams" do
+			@importer.teams.should be_instance_of Array
+		end
+
+		it "should return 18 teams" do
+			@importer.teams.count.should eql 18
+		end
+	end
+
+	describe "#matches" do
+
+		it "should return a list of matches" do
+			@importer.matches.should be_instance_of Array
+		end
+
+		it "should return 34 * 9 matches" do
+			@importer.matches.count.should eql (34 * 9)
+		end
+
+	end
+
+	describe "#request_params" do
+		it "should always contain league code and season" do
+			params = @importer.request_params
+			params.keys.should include :league_shortcut
+			params.keys.should include :league_saison
+		end
+
+		it "should merge in additional params" do
+			params = @importer.request_params({ foo: "bar" })
+			params.keys.should include :foo
+			params[:foo].should eql "bar"
+		end
+	end
+
+	describe "#parse_teams" do
+		it "should accept a json string and return team instances" do
+			json = JSON.parse('{"team":[{"team_id":"7","team_name":"Borussia Dortmund","team_icon_url":"http://www.openligadb.de/images/teamicons/Borussia_Dortmund.gif"},{"team_id":"131","team_name":"VfL Wolfsburg","team_icon_url":"http://www.openligadb.de/images/teamicons/VfL_Wolfsburg.gif"}]}')
+			teams = @importer.parse_teams json
+			teams.count.should eql 2
+			teams.first.name.should eql "Borussia Dortmund"
+		end
 	end
 
 end
