b541d82c6876a2b45f24e9b5d4ce0ad0d804f5f3
janpapenbrock <lunoxx@gmail.com>
2013-09-17 21:39:07 +0200

    Let run script run on random data.

--- run.rb
+++ run.rb
@@ -1,17 +1,26 @@
 # not runnable example
 
-matches = MatchImporter.importSeason(2012)
+require_relative "lib/unpredictable/unpredictable.rb"
+
+matches = SeasonGenerator.new(18).matches
 
 predictor = PredictionFactory::PreviousMatch.new
-evaluator = Evaluation::Kicktipp.new
-
+evaluator = Evaluation::Kicktipp
 
 predictions = matches.collect do |match|
 	predictor.predict match
 end
 
+predictions.compact!
+
 scores = predictions.collect do |prediction|
-	evaluator.evaluate prediction
+	puts prediction
+	eval = evaluator.new(prediction, prediction.match)
+	score = eval.evaluate 
+
+	puts score
+	score
 end
 
 total_score = scores.reduce(:+)
+p total_score
