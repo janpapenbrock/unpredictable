e363979ee85a9bff3d04ed0f0eb26dd19a768861
janpapenbrock <lunoxx@gmail.com>
2013-09-17 21:37:49 +0200

    Move knowledge of classes from spec to lib.

---
+++ lib/unpredictable/unpredictable.rb
@@ -0,0 +1,7 @@
+require_relative "core/match.rb"
+require_relative "core/match_importer.rb"
+require_relative "core/prediction.rb"
+require_relative "core/season_generator.rb"
+require_relative "core/team.rb"
+require_relative "evaluation/kicktipp.rb"
+require_relative "prediction_factory/previous_match.rb"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,4 @@
 require 'coveralls'
 Coveralls.wear!
 
-require_relative "../lib/unpredictable/core/match"
-require_relative "../lib/unpredictable/core/match_importer"
-require_relative "../lib/unpredictable/core/prediction"
-require_relative "../lib/unpredictable/core/team"
-require_relative "../lib/unpredictable/evaluation/kicktipp"
-require_relative "../lib/unpredictable/prediction_factory/previous_match"
+require_relative "../lib/unpredictable/unpredictable"
