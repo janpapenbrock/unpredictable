902a13c6686bbe707cdc618806c67be54c019c1b
janpapenbrock <lunoxx@gmail.com>
2013-09-22 12:09:53 +0200

    Prepare for using and comparing multiple prediction factories.

--- run.rb
+++ run.rb
@@ -4,21 +4,25 @@ require_relative "lib/unpredictable/unpredictable.rb"
 
 matches = MatchImporter.new("bl1", 2012).matches
 
-predictor = PredictionFactory::PreviousMatch.new
 evaluator = Evaluation::Kicktipp
+predictors = [ PredictionFactory::PreviousMatch.new ]
 
-predictions = matches.collect do |match|
-	predictor.predict match
+scores = predictors.collect do |predictor|
+	predictions = matches.collect do |match|
+		predictor.predict match
+	end
+
+	predictions.compact!
+
+	scores = predictions.collect do |prediction|
+		eval = evaluator.new(prediction, prediction.match)
+		score = eval.evaluate 
+
+		score
+	end
+
+	total_score = scores.reduce(:+)
+	[predictor.class.name, total_score]
 end
 
-predictions.compact!
-
-scores = predictions.collect do |prediction|
-	eval = evaluator.new(prediction, prediction.match)
-	score = eval.evaluate 
-
-	score
-end
-
-total_score = scores.reduce(:+)
-p total_score
+p scores
